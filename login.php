7ade1e79ac4c550e2997259bcb73bee8c5f3a48b
Sunny-Ojo <sunny.ojo.14@gmail.com>
2019-09-20 09:18:16 +0100

    made some validation changes

--- login.php
+++ login.php
@@ -1,40 +1,44 @@
+<?php 
+session_start();
+if(isset($_POST['submit'])){
+// header('location: index.php');
+$con = mysqli_connect('localhost','root','');
+mysqli_select_db($con, 'hng_task2');
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$s = " SELECT * FROM reg_user WHERE email = '$email' && password ='$password'";
+$result = mysqli_query($con, $s);
+$num = mysqli_num_rows($result);
+if ($num == 1) {
+	$_SESSION['username'] = $username;
+	header('location: dashboard.php');
+}else{
+header('location:signup.php');
+}
+}
+ ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <meta
-      name="description"
-      content="A simple login and sign-up form using html, css and bootstrap"
-    />
-    <meta
-      name="keywords"
-      content="login form, sign-up form, simple login and sign-up page"
-    />
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/style.css" />
-    <title>Register</title>
+    <title>Register | Welcome</title>
   </head>
   <body>
     <div class="container">
       <div class="row">
         <div class="col-md-5 offset-md-6 form-div">
           <h1 class="text-lg-center alert-danger"><b>TEAM SMOOTH</b></h1>
-          <form action="#" method="post">
-            <h3 class="text-center">Register</h3>
 
-            <div class="form-group">
-              <label for="username">Username:</label>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                class="form-control form-control-lg"
-                placeholder="Example: Sunny Melvin"
-                required
-              />
-            </div>
+          <form action="login.php" method="post">
+            <h3 class="text-center">Login</h3>
             <div class="form-group">
               <label for="email">Email:</label>
               <input
@@ -42,8 +46,7 @@
                 name="email"
                 id="email"
                 class="form-control form-control-lg"
-                required
-                placeholder="Example: me@gmail.com"
+                placeholder="Enter Your Email"
               />
             </div>
 
@@ -54,34 +57,22 @@
                 name="password"
                 id="password"
                 class="form-control form-control-lg"
-                placeholder="Example: *****"
-                required
-                visibility
+                placeholder="Enter Your password"
               />
             </div>
-            <div class="form-group">
-              <label for="password_confirm">Confirm Password:</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                class="form-control form-control-lg"
-                placeholder="Example: *****"
-                required
-              />
-            </div>
+
             <div class="form-group">
               <button
                 type="submit"
                 name="submit"
                 class="btn btn-primary btn-lg btn-block"
               >
-                Submit
+                Sign In
               </button>
             </div>
             <p class="text-center">
-              Already a Member?
-              <b><a class="signup_btn" href="login.html">Sign In</a></b>
+              Don't Have An Account?
+              <b><a class="signup_btn" href="signup.html">Sign Up</a></b>
             </p>
           </form>
         </div>
